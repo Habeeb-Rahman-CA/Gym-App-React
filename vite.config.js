73c9dc0d8052482e75ee5870228ced5b27ddb566
HabeebR07 <habeebrahmanca07@gmail.com>
2024-10-29 12:58:12 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Liftup - Hxbizz',
+        short_name: 'Liftup',
+        description: 'Elevate your fitness journey',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/192logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
