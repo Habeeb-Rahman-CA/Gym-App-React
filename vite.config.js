240b2546e44af76571a51115b9eea48b29a59720
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2026-02-27 14:48:08 +0530

    feat: pwa implementation

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Hxbizz LiftUp Gym App',
+        short_name: 'LiftUp',
+        description: 'Elevate Your Fitness Journey with Hxbizz LiftUp',
+        theme_color: '#151515',
+        background_color: '#151515',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   base: './',
   build: {
